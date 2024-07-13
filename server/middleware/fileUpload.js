dd8498d303f6fcdc871b224b18ae209a62b61837
Abdullah al rafi <abdullah116632@gmail.com>
2024-07-13 10:21:47 +0600

    file upload problem solve

--- server/middleware/fileUpload.js
+++ server/middleware/fileUpload.js
@@ -1,8 +1,13 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import multer from "multer";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const userStorage = multer.diskStorage({
     destination: function (req, file, cb){
-        cb(null, "server/fileData")
+        cb(null, path.resolve(__dirname, "..", "fileData"))
     },
     filename: function (req, file, cb){
         cb(null, Date.now() + "-" + file.originalname)
